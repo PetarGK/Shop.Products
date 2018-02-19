d097a0ce8562dbf58bbaedac4c8dd166a3e99b4a
AD\petar.korudzhiev <petar.korudzhiev@mentormate.com>
2018-02-19 13:04:37 +0200

    Initial commit

---
+++ Shop.Products/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shop.Products.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "value1", "value2", "value3" };
+        }
+
+        [HttpGet("HealthCheck")]
+        public string HealthCheck()
+        {
+            return "Service is healthy";
+        }
+
+        // GET api/products/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/products
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/products/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/products/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
